1bd311f339287ca6d5dc686a9d20cacba4c27561
иван кузьмич <ivankuzmic210@gmail.com>
2020-10-04 23:34:17 +0300

    fix html

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function(){
     var includes = $('[data-include]');
-    //document.querySelector("[data-include]").innerText;
+
     jQuery.each(includes, function(){
-        var file = 'components' + $(this).data('include') + '.html';
+        var file = 'components/' + $(this).data('include') + '.html';
         $(this).load(file);
     });
 });
@@ -10,7 +10,6 @@ $(function(){
 $('.menu-btn').on('click', function(){
 	$('.menu-wrapper').slideToggle('slow');
 });
-
 
 
 let editPopupSwitcher = false;
--- js/modules/template-page.js
+++ js/modules/template-page.js
@@ -5,3 +5,12 @@ theme.coolUpdate();
 window.addEventListener('storage', function (event){
     theme.coolUpdate();
 })
+
+$(function(){
+    let includes = $('[data-include]');
+
+    jQuery.each(includes, function(){
+        let file = '../components/' + $(this).data('include') + '.html';
+        $(this).load(file);
+    });
+});
